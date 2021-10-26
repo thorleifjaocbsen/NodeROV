daee72cf23b1b06327d80817bdf36cb3378836da
Thorleif Jacobsen <thorleif@tjweb.no>
2021-10-26 20:06:32 +0100

    Cleanup client for improvement

--- client/js/controls.js
+++ client/js/controls.js
@@ -1,108 +1,108 @@
 
-Controls = function() {
+Controls = function () {
   var self = {
-    gp      : null,
-    
-    forward : 0, // [-100,+100] Forward and reverse
-    strafe  : 0, // [-100,+100] Left and Right
-    yaw     : 0, // [-100,+100] Turn left, Turn right
-    climb   : 0, // [-100,+100] Up and down
+    gp: null,
 
-    map : {
-      gainUp : 12,
-      gainDown : 13,
-      lightsUp : 15,
-      lightsDown : 14,
-      cameraUp : 5,
-      cameraDown : 7,
-      fullscreen : 1,
-      depthhold : 0,
-	  gripClose : 6,
-      gripOpen : 4,
-      arm : 9,
-      disarm : 8,
+    forward: 0, // [-100,+100] Forward and reverse
+    strafe: 0, // [-100,+100] Left and Right
+    yaw: 0, // [-100,+100] Turn left, Turn right
+    climb: 0, // [-100,+100] Up and down
+
+    map: {
+      gainUp: 12,
+      gainDown: 13,
+      lightsUp: 15,
+      lightsDown: 14,
+      cameraUp: 5,
+      cameraDown: 7,
+      fullscreen: 1,
+      depthhold: 0,
+      gripClose: 6,
+      gripOpen: 4,
+      arm: 9,
+      disarm: 8,
     },
-    
-    callbacks : {},
-    debounce  : {},
-    repeatInterval  : {},
-    repeatIntervalTimer : {},
-    
-    lastUpdate  : 0,
-    changedSinceReturn : false,
-    warned : false,
+
+    callbacks: {},
+    debounce: {},
+    repeatInterval: {},
+    repeatIntervalTimer: {},
+
+    lastUpdate: 0,
+    changedSinceReturn: false,
+    warned: false,
   }
-  
-  self.init = function() {
+
+  self.init = function () {
     window.addEventListener("keydown", controls.keyDown);
     window.addEventListener("keyup", controls.keyUp);
   }
-  
-  self.checkGamepad = function() {
+
+  self.checkGamepad = function () {
     try { self.gp = navigator.getGamepads()[0]; return self.gp.connected; }
-    catch(err) { return false; }
+    catch (err) { return false; }
   }
-  
-  self.update = function() {
-    if(!self.checkGamepad()) return false;
-    
-    if(self.lastUpdate == self.gp.timestamp) return false;
+
+  self.update = function () {
+    if (!self.checkGamepad()) return false;
+
+    if (self.lastUpdate == self.gp.timestamp) return false;
     self.lastUpdate = self.gp.timestamp;
     self.changedSinceReturn = true;
-        
-    self.forward  = Math.round(self.gp.axes[1] * 100);
-    self.strafe   = -1 * Math.round(self.gp.axes[0] * 100);
-    self.yaw      = -1 * Math.round(self.gp.axes[2] * 100);
-    self.climb    = Math.round(self.gp.axes[3] * 100);
+
+    self.forward = Math.round(self.gp.axes[1] * 100);
+    self.strafe = -1 * Math.round(self.gp.axes[0] * 100);
+    self.yaw = -1 * Math.round(self.gp.axes[2] * 100);
+    self.climb = Math.round(self.gp.axes[3] * 100);
 
     // Deadband
-    if(Math.abs(self.forward) < 5) self.forward = 0;
-    if(Math.abs(self.strafe)  < 5) self.strafe  = 0;
-    if(Math.abs(self.yaw)     < 5) self.yaw     = 0;
-    if(Math.abs(self.climb)   < 5) self.climb   = 0;
-    
-    for(var btn in self.gp.buttons) {
-      if(self.gp.buttons[btn].pressed && !self.debounce[btn] && typeof self.callbacks[btn] == "function") {
+    if (Math.abs(self.forward) < 5) self.forward = 0;
+    if (Math.abs(self.strafe) < 5) self.strafe = 0;
+    if (Math.abs(self.yaw) < 5) self.yaw = 0;
+    if (Math.abs(self.climb) < 5) self.climb = 0;
+
+    for (var btn in self.gp.buttons) {
+      if (self.gp.buttons[btn].pressed && !self.debounce[btn] && typeof self.callbacks[btn] == "function") {
         self.callbacks[btn]();
         self.debounce[btn] = true;
-		if(self.repeatInterval[btn] > 0) {
-			self.repeatIntervalTimer[btn] = setInterval(self.callbacks[btn], self.repeatInterval[btn]);
-		}
+        if (self.repeatInterval[btn] > 0) {
+          self.repeatIntervalTimer[btn] = setInterval(self.callbacks[btn], self.repeatInterval[btn]);
+        }
       }
-      else { 
-	    self.debounce[btn] = false;
-		clearInterval(self.repeatIntervalTimer[btn]);
-	  }
+      else {
+        self.debounce[btn] = false;
+        clearInterval(self.repeatIntervalTimer[btn]);
+      }
     }
-    
+
   }
-  
-  self.onPress = function(btn, callback, bounceDelete) {
-	if(isNaN(bounceDelete)) bounceDelete = 0;
-	bounceDelete = parseInt(bounceDelete);
+
+  self.onPress = function (btn, callback, bounceDelete) {
+    if (isNaN(bounceDelete)) bounceDelete = 0;
+    bounceDelete = parseInt(bounceDelete);
     self.callbacks[btn] = callback;
-	self.repeatInterval[btn] = bounceDelete;
+    self.repeatInterval[btn] = bounceDelete;
   }
-  
-  self.keyDown = function(e) {
-    console.log("Key down: %d",e.keyCode);
+
+  self.keyDown = function (e) {
+    console.log("Key down: %d", e.keyCode);
   }
-  
-  self.keyUp = function(e) {
-    console.log("Key up: %d",e.keyCode);
+
+  self.keyUp = function (e) {
+    console.log("Key up: %d", e.keyCode);
   }
-  
-  self.returnObject = function() {
+
+  self.returnObject = function () {
     var ret = {};
-    for(var i in self) {
+    for (var i in self) {
       let t = typeof self[i];
-      if(t !== "function" && t != "object") {
+      if (t !== "function" && t != "object") {
         ret[i] = self[i];
       }
     }
     self.changedSinceReturn = false;
     return ret;
   }
-  
-  return self;  
+
+  return self;
 }
--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,11 @@
-var gui      = GUI();
+var gui = GUI();
 var controls = Controls();
-var socket   = Socket();
-var player   = new Player({});
-var rovData  = { };
+var socket = Socket();
+var player = new Player({});
+var rovData = {};
 var vacuumTest = false;
 var confirmWaterTight = false;
+
 
 /************************
  *
@@ -15,13 +16,13 @@ var confirmWaterTight = false;
  ************************/
 
 player.socket = Socket();
-player.socket.log = function(text) { gui.log("Video: "+text); }
-player.socket.onopen = function() { player.socket.send("REQUESTSTREAM"); };
-player.socket.onmessage = function(e) {
-  if(typeof e.data == "string") {
+player.socket.log = function (text) { gui.log("Video: " + text); }
+player.socket.onopen = function () { player.socket.send("REQUESTSTREAM"); };
+player.socket.onmessage = function (e) {
+  if (typeof e.data == "string") {
     gui.log(e.data, Date.now());
   };
-  if(player.skipMessages) { return; }
+  if (player.skipMessages) { return; }
   var frame = new Uint8Array(e.data);
   player.decode(frame);
 };
@@ -38,76 +39,28 @@ $(".fvideo").html(player.canvas);
 
 gui.init();
 gui.log("Initializing NodeROV GUI");
-gui.socket = socket;
 
-$("canvas").each(function() { $(this).get(0).height = $(this).height(); $(this).get(0).width = $(this).width(); });
-gui.accelCanvas            = $(".fvitals .accelerometer canvas").get(0);
-gui.compassCanvas          = $(".fvitals .compass .rose canvas").get(0);
+
+$("canvas").each(function () { $(this).get(0).height = $(this).height(); $(this).get(0).width = $(this).width(); });
+gui.accelCanvas = $(".fvitals .accelerometer canvas").get(0);
+gui.compassCanvas = $(".fvitals .compass .rose canvas").get(0);
 gui.dataGraphCanvasContext = $(".fdatagraphics canvas").get(0);
 
-
-gui.setButton(0, "LEFT LIGHT", function(e) {
-  if(gui.getButtonState(0)) {
-    socket.send("setlight 0 0");
-  }
-  else {
-    e.target.className = "selected";
-    socket.send("setlight 0 100");
-  }
-});
-gui.setButton(2, "RIGHT LIGHT", function(e) {
-  if(gui.getButtonState(2)) {
-    socket.send("setlight 1 0");
-  }
-  else {
-    e.target.className = "selected";
-    socket.send("setlight 1 100");
-  }
-});
-
-gui.setButton(1, "ARM", function(e) { socket.send("armtoggle"); });
-gui.setButton(4, "HEADING HOLD", function(e) { socket.send("headinghold"); });
-gui.setButton(5, "DEPTH HOLD", function(e) { socket.send("depthhold"); });
-gui.setButton(6, "RECORD", function(e) {
-  if(gui.getButtonState(6)) {
-    player.socket.send("REQUESTSTREAM");
-    gui.setButtonState(6,false);
-  }
-  else {
-    player.socket.send("REQUESTSTREAM RECORD");
-    gui.setButtonState(6,true);
-  }
-
-});
+gui.animateDataGraph();
+gui.drawAccelerometer(3, 5)
+gui.drawCompass(120);
+gui.animateScale(1, 50, 50 + " PSI");
+gui.animateScale(2, 30, 30 + " M");
+gui.animateScale(3, 30, 30 + " &deg;");
 
 
-gui.setButton(7, "FULLSCREEN", function(e) {
-  $("section.video").toggleClass("fullscreen");
-  gui.setButtonState(7, $("section.video").hasClass("fullscreen"));
-  $("section.video").on("click", function() { gui.pressButton(7); });
-});
-gui.setButton(11, "NEXT ROW ->", function(e) { $(".buttonarray").toggleClass("nextRow"); });
-gui.setButton(21, "<- PREV ROW", function(e) { $(".buttonarray").toggleClass("nextRow"); });
-gui.setButton(12, "<- PREV ROW", function(e) { $(".buttonarray").toggleClass("nextRow"); });
-gui.setButton(13, "SET FLAT", function(e) { socket.send("setflat"); });
-gui.setButton(14, "CALIBRATE GYRO", function(e) { socket.send("calibrategyro"); });
-gui.setButton(24, "ADD EVENT", function(e) {
-  var msg = "<p>Enter message: <input id='eventmsg' type='text' value='' /></p>";
-  popup("Add event", msg, "Add", "Cancel", function() {
-    gui.log("Custom event: "+$("#eventmsg").val());
-    popup_hide();
-  });
-  $("#eventmsg").focus();
-});
-gui.setButton(25, "SCREENSHOT", function(e) {
-  var data = $(".fvideo canvas").get(0).toDataURL("image/jpeg", 1);
-  var filename = "noderov_"+(Date.now()/1000)+".jpg";
-  gui.log("Screenshot saved ("+filename+")");
-  $("<a download='" + filename + "' href='" + data + "'></a>")[0].click();
+let d = new Date().toISOString();
+$("time:first").html(d.split('T')[1].split('.')[0] + " UTC");
+$("time:last").html(d.split('T')[0]);
 
-});
+// gui.drawAccelerometer(rovData.pitch, rovData.roll);
 
-
+requestAnimationFrame(systemLoop);
 gui.setInfo(1, 0, "Int. temp:");
 gui.setInfo(2, 0, "Int. pressure:");
 gui.setInfo(3, 0, "Ext. temp:");
@@ -128,18 +81,18 @@ gui.setInfo(12, 0, "Ping:");
  *
  *
  ************************/
-controls.onPress(controls.map.gainUp,     function() { socket.send("setgain "+(rovData.gain+50)); });
-controls.onPress(controls.map.gainDown,   function() { socket.send("setgain "+(rovData.gain-50)); });
-controls.onPress(controls.map.lightsUp,   function() { socket.send("setlight 0 "+(rovData.lights[0]+10)); socket.send("setlight 1 "+(rovData.lights[1]+10)); }, 100);
-controls.onPress(controls.map.lightsDown, function() { socket.send("setlight 0 "+(rovData.lights[0]-10));  socket.send("setlight 1 "+(rovData.lights[1]-10)); }, 100);
-controls.onPress(controls.map.cameraUp,   function() { socket.send("setcamera "+(rovData.cameraPosition-1)); }, 10);
-controls.onPress(controls.map.cameraDown, function() { socket.send("setcamera "+(rovData.cameraPosition+1)); }, 10);
-controls.onPress(controls.map.fullscreen, function() { gui.pressButton(7); })
-controls.onPress(controls.map.arm,        function() { socket.send("arm"); });
-controls.onPress(controls.map.disarm,     function() { socket.send("disarm"); });
-controls.onPress(controls.map.depthhold,  function() { socket.send("depthhold"); });
-controls.onPress(controls.map.gripOpen,   function() { socket.send("gripopen"); } , 50);
-controls.onPress(controls.map.gripClose,  function() { socket.send("gripclose"); }, 50);
+controls.onPress(controls.map.gainUp, function () { socket.send("setgain " + (rovData.gain + 50)); });
+controls.onPress(controls.map.gainDown, function () { socket.send("setgain " + (rovData.gain - 50)); });
+controls.onPress(controls.map.lightsUp, function () { socket.send("setlight 0 " + (rovData.lights[0] + 10)); socket.send("setlight 1 " + (rovData.lights[1] + 10)); }, 100);
+controls.onPress(controls.map.lightsDown, function () { socket.send("setlight 0 " + (rovData.lights[0] - 10)); socket.send("setlight 1 " + (rovData.lights[1] - 10)); }, 100);
+controls.onPress(controls.map.cameraUp, function () { socket.send("setcamera " + (rovData.cameraPosition - 1)); }, 10);
+controls.onPress(controls.map.cameraDown, function () { socket.send("setcamera " + (rovData.cameraPosition + 1)); }, 10);
+controls.onPress(controls.map.fullscreen, function () { gui.pressButton(7); })
+controls.onPress(controls.map.arm, function () { socket.send("arm"); });
+controls.onPress(controls.map.disarm, function () { socket.send("disarm"); });
+controls.onPress(controls.map.depthhold, function () { socket.send("depthhold"); });
+controls.onPress(controls.map.gripOpen, function () { socket.send("gripopen"); }, 50);
+controls.onPress(controls.map.gripClose, function () { socket.send("gripclose"); }, 50);
 
 /************************
  *
@@ -149,22 +102,22 @@ controls.onPress(controls.map.gripClose,  function() { socket.send("gripclose");
  *
  ************************/
 
- var voltWarnLevel = 0;
+var voltWarnLevel = 0;
 
 socket.connect(location.hostname, 8080);
-socket.log = function(text) { gui.log("WebSocket: "+text); }
-socket.on("hb", function(time) {
+socket.log = function (text) { gui.log("WebSocket: " + text); }
+socket.on("hb", function (time) {
   time = time.split(" ");
-  socket.send("hb "+time[0]);
+  socket.send("hb " + time[0]);
   gui.setInfo(12, time[1])
 })
-socket.on("telemetryData", function(data) {
+socket.on("telemetryData", function (data) {
   rovData = JSON.parse(data);
 
   gui.setInfo(1, parseFloat(rovData.inside.temp).toFixed(2))
-  gui.setInfo(2, parseFloat(rovData.inside.pressure/1000*14.5037738).toFixed(2))
+  gui.setInfo(2, parseFloat(rovData.inside.pressure / 1000 * 14.5037738).toFixed(2))
   gui.setInfo(3, parseFloat(rovData.outside.temp).toFixed(2))
-  gui.setInfo(4,parseFloat(rovData.outside.pressure/1000*14.5037738).toFixed(2))
+  gui.setInfo(4, parseFloat(rovData.outside.pressure / 1000 * 14.5037738).toFixed(2))
 
   gui.setInfo(5, parseFloat(rovData.inside.coreTemp).toFixed(2))
   gui.setInfo(6, parseInt(rovData.mAmpUsed))
@@ -177,50 +130,50 @@ socket.on("telemetryData", function(data) {
 
 
   // Update lights gui
-  for(i in rovData.lights) { gui.setButtonState(2*i, rovData.lights[i] > 0); }
+  for (i in rovData.lights) { gui.setButtonState(2 * i, rovData.lights[i] > 0); }
 
   // Update armed gui
   gui.setButtonState(1, rovData.armed);
   gui.setButtonState(5, rovData.depth.hold);
   gui.setButtonState(4, rovData.heading.hold);
 
-  var insidePressure = parseFloat(rovData.inside.pressure/1000*14.5037738).toFixed(2);
-  if(insidePressure < 14 && !vacuumTest) {
-    popup("Vacuum test - Stage 1", "<p>Starting automatic vacuum test due to lower internal pressure.<br />Be sure the LiPo battery is not inside and you use a vacuum safe battery.<br /><br />Current pressure is: <span class='currpress'>"+insidePressure+"</span></p>");
+  var insidePressure = parseFloat(rovData.inside.pressure / 1000 * 14.5037738).toFixed(2);
+  if (insidePressure < 14 && !vacuumTest) {
+    popup("Vacuum test - Stage 1", "<p>Starting automatic vacuum test due to lower internal pressure.<br />Be sure the LiPo battery is not inside and you use a vacuum safe battery.<br /><br />Current pressure is: <span class='currpress'>" + insidePressure + "</span></p>");
     gui.log("Vacuum Test: Stage 1 starting")
     vacuumTest = 1;
   }
-  else if(insidePressure < 12 && insidePressure > 4.5 && vacuumTest == 1) {
+  else if (insidePressure < 12 && insidePressure > 4.5 && vacuumTest == 1) {
     $(".currpress").html(insidePressure);
   }
-  else if(insidePressure <= 5 && vacuumTest == 1) {
-    popup("Vacuum test - Stage 2", "<p>Vacuum test at wanted pressure. Stop pumping now:<br /><br />Time passed: <span class='timepassed'>0</span> sec of 900<br />Current pressure is: <span class='currpress'>"+insidePressure+"</span></p>");
+  else if (insidePressure <= 5 && vacuumTest == 1) {
+    popup("Vacuum test - Stage 2", "<p>Vacuum test at wanted pressure. Stop pumping now:<br /><br />Time passed: <span class='timepassed'>0</span> sec of 900<br />Current pressure is: <span class='currpress'>" + insidePressure + "</span></p>");
     gui.log("Vacuum Test: Stage 2 starting")
     vacuumTest = Date.now();
   }
-  else if(vacuumTest && insidePressure > 14) {
+  else if (vacuumTest && insidePressure > 14) {
     popup_hide();
     vacuumTest = false;
     gui.log("Vacuum Test: Cancelled")
   }
-  if(vacuumTest >= 4) {
+  if (vacuumTest >= 4) {
     var passed = (Date.now() - vacuumTest) / 1000;
     $(".timepassed").html(Math.round(passed));
     $(".currpress").html(insidePressure);
 
-    if(passed > 900 && insidePressure <= 5) {
+    if (passed > 900 && insidePressure <= 5) {
       vacuumTest = 3;
-      gui.log("Vacuum Test: Passed, pressure after 15 minutes are "+insidePressure+" PSI");
+      gui.log("Vacuum Test: Passed, pressure after 15 minutes are " + insidePressure + " PSI");
 
-      popup("Vacuum test - Passed", "<p>ROV seems tight, internal pressure did not go above 5PSI in the period of 60 seconds.<br /><br />Release pressure before you continue then press 'Confirm'</p>", "Confirm", false, function() {
+      popup("Vacuum test - Passed", "<p>ROV seems tight, internal pressure did not go above 5PSI in the period of 60 seconds.<br /><br />Release pressure before you continue then press 'Confirm'</p>", "Confirm", false, function () {
         vacuumTest = false;
         popup_hide();
       });
     }
-    else if(insidePressure > 5) {
+    else if (insidePressure > 5) {
       vacuumTest = 3;
-      gui.log("Vacuum Test: FAILED, pressure after "+(passed/60)+" minutes are "+insidePressure+" PSI");
-      popup("Vacuum test - Fail", "<p>ROV seems leaky, internal pressure did pass 4.5PSI during the test period.<br />It hit "+insidePressure+" in only "+passed+" seconds..<br /><br />Release pressure before you continue then press 'Confirm' then check for leaks</p>", "Confirm", false, function() {
+      gui.log("Vacuum Test: FAILED, pressure after " + (passed / 60) + " minutes are " + insidePressure + " PSI");
+      popup("Vacuum test - Fail", "<p>ROV seems leaky, internal pressure did pass 4.5PSI during the test period.<br />It hit " + insidePressure + " in only " + passed + " seconds..<br /><br />Release pressure before you continue then press 'Confirm' then check for leaks</p>", "Confirm", false, function () {
         popup_hide();
         vacuumTest = false;
       });
@@ -228,22 +181,22 @@ socket.on("telemetryData", function(data) {
   }
 
   // On Screen Warnings:
-  if(rovData.volt < 10.5 && voltWarnLevel == 0) {
+  if (rovData.volt < 10.5 && voltWarnLevel == 0) {
     gui.overlayText("Voltage warning", 3);
     voltWarnLevel = 1;
   }
-  else if(rovData.volt < 9.8 && voltWarnLevel == 1) {
+  else if (rovData.volt < 9.8 && voltWarnLevel == 1) {
     gui.overlayText("Voltage warning", 3);
     voltWarnLevel = 2;
   }
-  else if(rovData.volt < 9 && voltWarnLevel == 2) {
+  else if (rovData.volt < 9 && voltWarnLevel == 2) {
     gui.overlayText("Battery dying", 100);
     voltWarnLevel = 3;
   }
 
 
 });
-socket.on("log", function(data) { data = JSON.parse(data); gui.log(data.message, data.time, true); })
+socket.on("log", function (data) { data = JSON.parse(data); gui.log(data.message, data.time, true); })
 
 
 /************************
@@ -255,43 +208,9 @@ socket.on("log", function(data) { data = JSON.parse(data); gui.log(data.message,
  ************************/
 
 function systemLoop() {
-  if(controls.changedSinceReturn) {
-    socket.send('controls ' + JSON.stringify(controls.returnObject()));
-  }
-
-  // ! < add it below
-  if(!controls.checkGamepad() && controls.warned) {
-    popup("Connect Gamepad", "Please connect the gamepad to continue.");
-    controls.warned = true;
-  }
-  else if(controls.checkGamepad() && controls.warned) {
-    popup_hide();
-    controls.warned = false;
-  }
-  else if(confirmWaterTight == false && controls.warned == false) {
-    //popup("Confirm water tightness of chambers", "Check for loose connectors and that vacuum plugs are connected before pressing confirm!", "Confirm");
-    confirmWaterTight = true;
-  }
-
-  if(controls.checkGamepad()) controls.update();
-  gui.animateDataGraph();
-  if(rovData.heading) {
-    gui.drawCompass(rovData.heading.current);
-  }
-
-
-  if(rovData.outside) {
-    var PSI = parseFloat(rovData.outside.pressure/1000*14.5037738).toFixed(2);
-    gui.animateScale(1, gui.map(parseInt(PSI), 0, 1450, 0, 100), PSI+" PSI");
-    gui.animateScale(2, parseInt(rovData.outside.depth), rovData.outside.depth.toFixed(2)+" M");
-    gui.animateScale(3, gui.map(parseInt(rovData.outside.temp), -30, 30, 0, 100), rovData.outside.temp.toFixed(2)+" &deg;");
-  }
-
   let d = new Date().toISOString();
-  $("time:first").html(d.split('T')[1].split('.')[0]+" UTC");
+  $("time:first").html(d.split('T')[1].split('.')[0] + " UTC");
   $("time:last").html(d.split('T')[0]);
-
-  gui.drawAccelerometer(rovData.pitch,rovData.roll);
 
   requestAnimationFrame(systemLoop);
 }
@@ -302,22 +221,22 @@ systemLoop();
 
 
 function popup(title, message, button1, button2, button1_callback, button2_callback) {
-  if(!button1_callback && button1) {
+  if (!button1_callback && button1) {
     button1_callback = popup_hide;
   }
-  if(!button2_callback && button2) {
+  if (!button2_callback && button2) {
     button2_callback = popup_hide;
   }
 
   $(".msgbox button").css("display", "none");
   $(".msgbox button").off();
 
-  if(button1) {
+  if (button1) {
     $(".msgbox button:first").html(button1);
     $(".msgbox button:first").on('click', button1_callback);
     $(".msgbox button:first").css("display", "");
   }
-  if(button2) {
+  if (button2) {
     $(".msgbox button:last").html(button2);
     $(".msgbox button:last").on('click', button2_callback);
     $(".msgbox button:last").css("display", "");
@@ -328,7 +247,7 @@ function popup(title, message, button1, button2, button1_callback, button2_callb
   $(".msgbox").fadeIn();
   $(".msgbox-bg").fadeIn();
 
-  $(".msgbox").css("margin-top", $(".msgbox").height()*-1);
+  $(".msgbox").css("margin-top", $(".msgbox").height() * -1);
 }
 
 function popup_hide() {
