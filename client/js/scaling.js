11fe3efc85d656313a37712fd841e8eff29bac59
Thorleif Jacobsen <thorleif@tjweb.no>
2022-04-13 18:14:35 +0100

    Removed video scaling code from the full screen scaling.

--- client/js/scaling.js
+++ client/js/scaling.js
@@ -3,25 +3,17 @@
  ************************/
 
 const containerEl = document.getElementById('container');
-const videoEl = document.getElementById('video');
 const containerMaxWidth = containerEl.offsetWidth;
 const containerMaxHeight = containerEl.offsetHeight;
-const videoMaxWidth = videoEl.offsetWidth;
-const videoMaxHeight = videoEl.offsetHeight;
+
 function resize() {
     const sWidth = window.innerWidth;
     const sHeight = window.innerHeight;
     containerEl.style.zoom = Math.min(sWidth / containerMaxWidth, sHeight / containerMaxHeight);
-
-    // Get canvas inside of video element
-    const canvasEl = videoEl.getElementsByTagName('canvas')[0];
-
-    if (canvasEl) {
-        canvasEl.style.zoom = Math.min(videoMaxWidth / canvasEl.width, videoMaxHeight / canvasEl.height);
-    }
 };
 
 resize();
+
 window.addEventListener("resize", resize);
 
 
