fe958e19dade683080ff603c8f1bafa2a45c8f75
Thorleif Jacobsen <thorleif@tjweb.no>
2022-04-18 16:20:44 +0100

    Improved linegraph

--- client/js/classes/LineChart.js
+++ client/js/classes/LineChart.js
@@ -35,7 +35,7 @@ export default class LineChart {
   }
 
   setDepthScale(depthScale) {
-    this.depthScale = depthScale;
+    this.depthScale = depthScale + this.#seaLevelOffset;
     this.pixelsPerCentimeter = this.#height / (this.depthScale * 100);
   }
 
@@ -58,19 +58,24 @@ export default class LineChart {
       // Remove if diff is bigger than the time scale
       if (diff > this.timeScale) {
         this.#points.splice(index, 1);
-      } 
+      }
 
       // Find the deepets point 
       if (point.depth > deepest) { deepest = point.depth; }
     });
 
     // Update depth scale if needed     
-    if(deepest < 5) deepest = 5;
+    if (deepest < 5) deepest = 5;
     this.setDepthScale(deepest);
   }
 
-
   draw() {
+    this.drawPoints();
+    this.drawDepthScale();
+  }
+
+
+  drawPoints() {
     this.#ctx.clearRect(0, 0, this.#width, this.#height);
     this.#ctx.save();
 
@@ -83,10 +88,10 @@ export default class LineChart {
 
     // Draw lines between each point.
     this.#points.forEach(point => {
-      
+
       // Calculate y axis position
       let yPosition = (point.depth * 100) * this.pixelsPerCentimeter;
-      
+
       // Calculate x axis position
       let timeDiff = (point.time - lastPointsTime) / 1000;  // In seconds
       let xPosition = timeDiff * this.pixelsPerSecond;
@@ -98,11 +103,46 @@ export default class LineChart {
     this.#ctx.stroke();
 
     this.#ctx.beginPath();
-    this.#ctx.strokeStyle = "#51861d";
+    this.#ctx.strokeStyle = "rgba(0,255,0,0.1)";
     this.#ctx.moveTo(0, sealevel);
     this.#ctx.lineTo(this.#width, sealevel);
     this.#ctx.stroke();
     this.#ctx.restore();
+  }
+
+  drawDepthScale() {
+    console.log("DRAW");
+    // Common text formats
+    const offSet =  this.#seaLevelOffset * 100 * this.pixelsPerCentimeter;
+    let increment;
+    
+    if(this.depthScale < 10) { increment = 1; }
+    else if(this.depthScale < 20) { increment = 2; }
+    else if(this.depthScale < 50) { increment = 5; }
+    else if(this.depthScale < 100) { increment = 5; }
+    else { increment = 20; }
+
+    // Spread numbers of meters between depth scale
+    for (let i = 0; i < this.depthScale-1; i+=increment) {
+      let yPosition = (i * 100) * this.pixelsPerCentimeter + offSet;
+      this.writeText(i + "m", yPosition);
+    }
+    
+    //this.#ctx.fillStyle = "rgb(231,96,98)";
+    //this.#ctx.fillText(roll + "°", width - 20, centerY)
+
+  }
+
+
+
+  writeText(text, y, baseline = "middle") {
+    this.#ctx.font = "bold 15px Open Sans";
+    this.#ctx.textBaseline = baseline;
+
+    this.#ctx.textAlign = "left";
+
+    this.#ctx.fillStyle = "rgb(255,255,255)";
+    this.#ctx.fillText(text, 10, y);
   }
 
 
