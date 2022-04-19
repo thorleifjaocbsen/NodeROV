d269ae98d566a77a4cbe669d09eef0651912c1d5
Thorleif Jacobsen <thorleif@tjweb.no>
2022-04-19 13:17:37 +0100

    Cleaned up ADC code made it more usable

--- js/classes/AnalogDigitalConverter.js
+++ js/classes/AnalogDigitalConverter.js
@@ -6,100 +6,108 @@
 const EventEmitter = require('events')
 const ADS1015 = require('ads1015')
 
+const PGA = 4.096; // Power Gain Amplifier  - 4.096 on gain 1
+const MAX_RANGE = 2048; // ADS1015 - 2^(12-1) // 12bit, -2048 to 2047
+module.exports = class AnalogDigitalConverter extends EventEmitter {
 
-const PGA = 4.096 // Power Gain Amplifier  - 4.096 on gain 1
-const MAX_RANGE = 2048 // ADS1015 - 2^(12-1) // 12bit, -2048 to 2047
-
-
-module.exports = class AnalogDigitalConverter {
 
   constructor(options) {
 
-    this.eventEmitter = new EventEmitter()
+    super();
 
-    this.busNo = 1
-    this.address = 0x48
-    this.provider = 'i2c-bus'
+    this.busNo = 1;
+    this.address = 0x48;
+    this.provider = 'i2c-bus';
 
     ADS1015.open(this.busNo, this.address, this.provider)
-    .then(ads1015 => {
-      this.sensor = ads1015
-      this.sensor.gain = 1
-      this.readSensorData()
-      this.eventEmitter.emit('init')
-    })
+      .then(ads1015 => {
+        this.sensor = ads1015
+        this.sensor.gain = 1
+        this.readSensorData()
+        super.emit('init')
+      });
 
 
-    this.voltageDividor = (options && options.hasOwnProperty('voltageDividor')) ? options.voltageDividor : 0.176029962546816;    
-    this.currentMultiplier = (options && options.hasOwnProperty('currentMultiplier')) ? options.currentMultiplier : 35.714285714285715;    
+    this.voltageDividor = (options && options.hasOwnProperty('voltageDividor')) ? options.voltageDividor : 0.176029962546816;
+    this.currentMultiplier = (options && options.hasOwnProperty('currentMultiplier')) ? options.currentMultiplier : 35.714285714285715;
 
     // Default values = 0
-    this.a2 = 0
-    this.a0 = 0
-    this.a1 = 0
+    this.data = {
+      a0: 0,
+      a1: 0,
+      a2: 0
+    };
 
     // Auto Read Sensor 
-    this.autoRead = true
-    this.readInterval = 500
-    this.lastRead = null
-    this.lastMAH = null
-    this.accumulatedCurrent = 0
-  }
-
-
-  on(event, callback) {
-
-    this.eventEmitter.on(event, callback)
+    this.autoRead = true;
+    this.readInterval = 250;
+    this.lastRead = null;
+    this.lastMAH = null;
+    this.accumulatedCurrent = 0;
   }
 
 
   async readSensorData() {
 
-    this.a0 = (await this.sensor.measure('0+GND') / MAX_RANGE * PGA)
-    this.a1 = (await this.sensor.measure('1+GND') / MAX_RANGE * PGA)
-    this.a2 = (await this.sensor.measure('2+GND') / MAX_RANGE * PGA)
-    
-    this.calculateAccumulatedMah()
+    this.data.a0 = (await this.sensor.measure('0+GND') / MAX_RANGE * PGA);
+    this.data.a1 = (await this.sensor.measure('1+GND') / MAX_RANGE * PGA);
+    this.data.a2 = (await this.sensor.measure('2+GND') / MAX_RANGE * PGA);
 
-    this.eventEmitter.emit('read')
+    this.calculateAccumulatedMah();
 
-    if (this.autoRead) setTimeout(() => { this.readSensorData() }, this.readInterval)
+    super.emit('read');
+
+    if (this.autoRead) setTimeout(() => { this.readSensorData() }, this.readInterval);
   }
 
-  getCurrent() {
-    return this.a0 * this.currentMultiplier
+
+  getCurrent(digits = 2) {
+
+    digits = 10 ** digits
+    return Math.round((this.data.a0 / this.currentMultiplier) * digits) / digits;
   }
+
 
   getCurrentInMah() {
-    return this.getCurrent() * 1000
+
+    return this.getCurrent() * 1000;
   }
 
-  getVoltage() {
-    return this.a1 / this.voltageDividor
+
+  getVoltage(digits = 1) {
+
+    digits = 10 ** digits
+    return Math.round((this.data.a1 / this.voltageDividor) * digits) / digits;
   }
+
 
   getLeak() {
-    return this.a2 > 1
+
+    return this.data.a2 > 1;
   }
+
 
   getAccumulatedMah() {
-    return Math.round(this.accumulatedCurrent)
+
+    return Math.round(this.accumulatedCurrent);
   }
+
 
   calculateAccumulatedMah() {
-    if(this.lastRead == null) {
-      this.lastRead = Date.now()
-      this.lastMAH = this.getCurrentInMah()
-      this.accumulatedCurrent = 0
-      return
+
+    if (this.lastRead == null) {
+      this.lastRead = Date.now();
+      this.lastMAH = this.getCurrentInMah();
+      this.accumulatedCurrent = 0;
+      return;
     }
 
-    const duration = Date.now() - this.lastRead
-    this.accumulatedCurrent += (this.lastMAH / 3600000) * duration
-    this.lastRead = Date.now()
-    this.lastMAH = this.getCurrentInMah()
+    const duration = Date.now() - this.lastRead;
+    this.accumulatedCurrent += (this.lastMAH / 3600000) * duration;
+    this.lastRead = Date.now();
+    this.lastMAH = this.getCurrentInMah();
   }
 
-  
+
 
 }
