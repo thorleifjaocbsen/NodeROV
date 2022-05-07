8c6dc03e6d90f8b5c98926669740125a3fba5dd5
Thorleif Jacobsen <thorleif@tjweb.no>
2022-05-07 21:25:31 +0100

    Converted PCA9685 to use promises

--- js/drivers/PCA9685.js
+++ js/drivers/PCA9685.js
@@ -2,78 +2,98 @@ const i2cbus = require('i2c-bus');
 
 module.exports = class PCA9685 {
 
-  constructor(address, device) {  
-    this.address = (typeof address !== 'undefined') ? address : 0x40;
-    this.device = (typeof bus !== 'undefined') ? device : 1;
-    this.frequency = 50;
+  #sensor;
+  #frequency = 50;
+  #address = 0x40;
+  #device = 1;
 
-    this.i2c = i2cbus.openSync(this.device);
+  constructor(address, device) {
+    if (address) this.#address = address;
+    if (device) this.#device = device;
   }
 
   sleep(ms) {
-    const waitTill = new Date(new Date().getTime() + ms);
-    while (waitTill > new Date()) { }
-    return true;
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  setFrequency(frequency) {
+
+    return new Promise((resolve, reject) => {
+      if (this.#sensor) reject("Already initialized");
+      else {
+        this.#frequency = frequency;
+        resolve();
+      }
+    });
   }
 
   // Initializer, needs to be run after changing frequency
   init() {
-    // Calculate the prescaler based on frequency
-    let prescaler = Math.round(25000000 / (4096 * this.frequency)) - 1;
-    // Set all PWM to 0
-    this.setAllPWM(0);
-    // Set all outputs to use a totem pole structure.
-    this.send(0x01, 0x04); // MODE2 -> OUTDRV 
-    // Set it to respond on call on all i2c busses and sleep (Oscillator off) 
-    this.send(0x00, 0x11) // MODE1 -> ALLCALL, SLEEP
-    // Sleep 50µS so the oscillator can calm down
-    this.sleep(1);
-    // Set the prescale to precalculated scale
-    this.send(0xFE, prescaler); // PRE_SCALE -> pre-calculated
-    // Disable sleep so it can start up the oscillator again 
-    this.send(0x00, 0x01); // MODE1 -> ALLCALL   
-    // Spee 50µs so the oscillator can start up
-    this.sleep(1);
-    return true;
+
+    // Calculate prescaler based on frequency 
+    const prescaler = Math.round(25000000 / (4096 * this.#frequency)) - 1;
+
+    return i2cbus.openPromisified(this.#device)
+      .then((i2c) => { this.#sensor = i2c; })
+      .catch((err) => { throw `Could not open i2c bus: ${err}`; })
+      // Set all PWM to 0
+      .then(() => this.setAllPWM(0))
+      .catch((err) => { throw `Could not set all pwm signals to 0: ${err}`; })
+      // Set all outputs to use a totem pole structure.
+      .then(() => this.send(0x01, 0x04)) // MODE2 -> OUTDRV 
+      .catch((err) => { throw `Could not set mode2 to OUTDRV: ${err}`; })
+      // Set it to respond on call on all i2c busses and sleep (Oscillator off) 
+      .then(() => this.send(0x00, 0x11)) // MODE1 -> ALLCALL, SLEEP
+      // Sleep 1ms so the oscillator can calm down
+      .then(() => this.sleep(1))
+      // Set the prescale to precalculated scale
+      .then(() => this.send(0xFE, prescaler)) // PRE_SCALE -> pre-calculated
+      .catch((err) => { throw `Could not set prescaler: ${err}`; })
+      // Disable sleep so it can start up the oscillator again 
+      .then(() => this.send(0x00, 0x01)) // MODE1 -> ALLCALL
+      .catch((err) => { throw `Could not set mode1 to ALLCALL: ${err}`; })
+      // Sleep 1ms so the oscillator can start up
+      .then(() => this.sleep(1))
   }
 
   turnOffPWM() {
-    this.send(0xFD, 0x10)
+    return this.send(0xFD, 0x10)
   }
 
   // Usage: setPWM(channel, microsecounds);
   setPWM(no, us) {
+    
     const steps = this.usToSteps(us);
     // 0x06 -> 0x09 (LED0_ON_L,LED0_ON_H,LED0_OFF_L,LED0_OFF_H)
     // The 4*NO changes it so it inclines depending on NO up to 15.    
 
     // Steps are maxmimum between 0 and 4096
     // no is between 0 and 15 
-    if (steps < 0 || steps > 4096) { return false; }
-    if (no < 0 || no > 15) { return false; }
+    if (steps < 0 || steps > 4096) { return Promise.reject(); }
+    if (no < 0 || no > 15) { return Promise.reject(); }
 
-    // Lets write the start high to step 0
-    this.send(0x06 + 4 * no, 0x00)
-    this.send(0x07 + 4 * no, 0x00)
-    // Then write the end high to steps calculated from usToSteps
-    this.send(0x08 + 4 * no, steps & 0xFF )
-    this.send(0x09 + 4 * no, steps >> 8)
-    return true;
+    return Promise.all([
+      this.send(0x06 + 4 * no, 0x00), // Lets write the start high to step 0
+      this.send(0x07 + 4 * no, 0x00),
+      this.send(0x08 + 4 * no, steps & 0xFF), // Then write the end high to steps calculated from usToSteps
+      this.send(0x09 + 4 * no, steps >> 8)
+    ]);
   }
 
   // Usage: setPWM(microsecounds);
   setAllPWM(us) {
     const steps = this.usToSteps(us);
-    // 0xFA -> 0xFD (ALL_LED_ON_L,ALL_LED_ON_H,ALL_LED_OFF_L,ALL_LED_OFF_H)
 
     // Steps are maxmimum between 0 and 4096
-    if (steps < 0 || steps > 4095) { return false; }
-    // Lets write the start high to step 0
-    this.send(0xFA, 0x00)
-    this.send(0xFB, 0x00)
-    // Then write the end high to steps calculated from usToSteps
-    this.send(0xFC, steps & 0xFF)
-    this.send(0xFD, steps >> 8)
+    if (steps < 0 || steps > 4095) { return Promise.reject(); }
+
+    // 0xFA -> 0xFD (ALL_LED_ON_L,ALL_LED_ON_H,ALL_LED_OFF_L,ALL_LED_OFF_H)
+    return Promise.all([
+      this.send(0xFA, 0x00), // Lets write the start high to step 0
+      this.send(0xFB, 0x00),
+      this.send(0xFC, steps & 0xFF), // Then write the end high to steps calculated from usToSteps
+      this.send(0xFD, steps >> 8)
+    ]);
   }
 
   // Calculate how many steps an specific µS can be
@@ -87,7 +107,7 @@ module.exports = class PCA9685 {
   }
 
   send(command, byte) {
-    this.i2c.writeByteSync(this.address, command, byte)
+    return this.#sensor.writeByte(this.#address, command, byte);
   }
 
 }
